e2e0ef0f0a92670c0fbc6fe9a9966186b7b0b90f
Amen Dashaev <dashaevtl95@gmail.com>
2025-09-14 23:07:57 +0200

    User, Media, Rating

---
+++ MRP/MRP/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRP.Model
+{
+    public class User
+    {
+
+        public User() { }
+
+        public User(string username, string password) {
+
+            Username = username;
+            Password = password;    
+        
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }  
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+
+    }
+}
